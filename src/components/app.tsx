46e7e8aa5b57b2901f0803a99c94211970d2b2a4
Maakep <schon.fredrik@gmail.com>
2018-05-28 22:35:30 +0200

    Initial typescript react setup

---
+++ src/components/app.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+
+export class App extends React.Component<null, null> {
+  constructor() {
+    super(null);
+  }
+  render() {
+    return (
+      <div>
+        App
+      </div>
+    );
+  }
+}
